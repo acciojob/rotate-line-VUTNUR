ac270e16d7764bab4b5b82b4580c771c4e9cad59
Vutnur Nethaji Goud <130832771+VUTNUR@users.noreply.github.com>
2023-05-23 18:55:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let div=document.createElement("div");
+div.id="line"
+div.style.width="200px"
+div.style.height="2px"
+div.style.backgroundColor="black"
+div.style.animation = "rotateAnimation 2s infinite";
+let body=document.getElementsByTagName("body")[0];
+body.append(div);
